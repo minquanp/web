9ff02bdcc98cb85f95f40f89be124d3d68913137
Pham Minh Quan <minhquanst129@gmail.com>
2026-03-21 11:08:48 +0700

    21/03

---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    let ext = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (ext) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('File not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+});
+
+server.listen(8000, () => {
+    console.log('Server running at http://localhost:8000');
+});
